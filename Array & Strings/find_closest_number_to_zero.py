b0c3d33a05ee3cacbd434f083d7c0db1798b5ba4
calvin phuah <calvinphuah73@gmail.com>
2025-03-24 22:36:10 +0800

    first commit

---
+++ Array & Strings/find_closest_number_to_zero.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        # Goal: Return the number closest to 0 using abs too
+        # Intuition: use modulo, keep a max
+        closest = nums[0]
+
+        for n in nums:
+            if abs(n) < abs(closest):
+                closest = n
+
+        # Create a check to see if there is a positive number
+        if closest < 0 and abs(closest) in nums:
+            return abs(closest)
+        else:
+            return closest
+
+    # Time: O(n) because we are loop through the whole input
+    # Space: O(1)
